defb8a720769ddc75dfd2c1743cc6d958e631308
Rithikis14 <rithikvkumar1475@gmail.com>
2026-09-10 14:05:40 +0530

    Setting up routes

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://ecolearn-sfhm.onrender.com/api/auth';
 
 interface User {
   id: string;
